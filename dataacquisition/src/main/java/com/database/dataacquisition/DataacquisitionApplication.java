758ca1ad47dceb7912249ff92218f9810dd3fe1f
huangjuping <huangjuping@viewshine.cn>
2018-04-25 09:47:27 +0800

    1、数据库监测与报警项目建立； 2、数据采集模块建立；

---
+++ dataacquisition/src/main/java/com/database/dataacquisition/DataacquisitionApplication.java
@@ -0,0 +1,12 @@
+package com.database.dataacquisition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataacquisitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataacquisitionApplication.class, args);
+	}
+}
